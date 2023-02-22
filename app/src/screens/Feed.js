1c068301ab0547d3944755ac8c2c086a34ff9096
EvanOptAxe <evan.oleary@optaxe.com>
2023-02-22 23:33:08 +0000

    Display items

--- app/src/screens/Feed.js
+++ app/src/screens/Feed.js
@@ -1,7 +1,19 @@
 import React from "react";
-import { Text } from "react-native";
+import { Text, View, ScrollView } from "react-native";
 import tw from 'twrnc';
 
 export default function Feed() {
-  return <Text style={tw`text-white bg-black text-xl`}>Test Content</Text>;
+  const recipes = require('../test-data/maximise-ingredients-recipes.json');
+
+
+  return (
+    <ScrollView>
+      {recipes.map(recipe =>
+        <View key={recipe.id}>
+          <Text>{recipe.title}</Text>
+        </View>
+      )
+      }
+    </ScrollView>
+  );
 }
