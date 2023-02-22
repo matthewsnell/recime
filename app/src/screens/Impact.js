1b5a515d63f420e8940bfce849ed01d32f103249
Tim Tommins <tom.timmons@outlook.com>
2023-02-22 23:58:37 +0000

    initail setup

--- app/src/screens/Impact.js
+++ app/src/screens/Impact.js
@@ -1,6 +1,22 @@
 import React from "react";
-import { Text } from "react-native";
+import { Button, SafeAreaView, Text } from "react-native";
+import {
+  LineChart,
+  BarChart,
+  PieChart,
+  ProgressChart,
+  ContributionGraph,
+  StackedBarChart
+} from "react-native-chart-kit";
 
 export default function Impact() {
-  return <Text>Test Content</Text>;
+  return (
+    <SafeAreaView>
+      <Text>Test Content</Text>
+      <Button 
+        onPress={() => {console.log("Hello world")}}
+        title="Hello world!"
+      />
+    </SafeAreaView>
+  );
 }
